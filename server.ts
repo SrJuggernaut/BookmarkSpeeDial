0aa93e98943d1229d191f74b55701a82a9d24a50
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-10 14:17:11 -0600

    fix: bun watch dangling processes when develop

--- server.ts
+++ server.ts
@@ -1,39 +1,20 @@
+import { $ } from 'bun'
 import { watch } from 'fs'
 import path from 'path'
-import { build } from 'vite'
 
-const buildReact = (): void => {
-  console.time('build')
-  build({
-    root: path.resolve(__dirname),
-    build: {
-      assetsInlineLimit: Infinity,
-      rollupOptions: {
-        makeAbsoluteExternalsRelative: 'ifRelativeSource'
-      },
-      cssMinify: true,
-      emptyOutDir: true
-    }
-  })
-    .catch(console.error)
-    .finally(() => {
-      console.timeEnd('build')
-    })
-}
+$`bun run build:nopack`.then(console.log).catch(console.error)
 
-buildReact()
+const controller = new AbortController()
 
-const watcher = watch(
-  path.resolve(__dirname, 'src'),
-  {
-    recursive: true
-  },
-  (event, filename) => {
-    console.log(`Detected ${event} in ${filename}`)
-    buildReact()
-  })
+const watcher = watch(path.resolve(__dirname, 'src'), { signal: controller.signal })
+
+watcher.on('change', (filename) => {
+  console.log(`Detected change in ${filename}`)
+  $`bun run build:nopack`.then(console.log).catch(console.error)
+})
 
 process.on('SIGINT', () => {
+  controller.abort()
   watcher.close()
   process.exit(0)
 })

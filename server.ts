bddc7152dc15c33bca65bbed4929c0efb1221f3b
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-12 20:02:55 -0600

    fix: check recursive for change in files while develop

--- server.ts
+++ server.ts
@@ -2,8 +2,6 @@ import { $ } from 'bun'
 import { watch } from 'fs'
 import path from 'path'
 
-$`bun run build:nopack`.then(console.log).catch(console.error)
-
 const build = async (): Promise<void> => {
   await $`rm -rf dist`.text()
   const exit = await $`bun run build:nopack`.text()
@@ -15,7 +13,7 @@ build()
 
 const controller = new AbortController()
 
-const watcher = watch(path.resolve(__dirname, 'src'), { signal: controller.signal })
+const watcher = watch(path.resolve(__dirname, 'src'), { signal: controller.signal, recursive: true })
 
 watcher.on('change', (change, filename) => {
   console.log(`Detected ${change} in ${typeof filename === 'string' ? filename : `Buffer : ${filename.toString()}`}`)

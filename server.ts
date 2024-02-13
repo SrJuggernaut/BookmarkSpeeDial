d82340cfe46771cfecb3735e2394defa00cdfd62
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-12 19:52:39 -0600

    fix: better development messages

--- server.ts
+++ server.ts
@@ -4,13 +4,23 @@ import path from 'path'
 
 $`bun run build:nopack`.then(console.log).catch(console.error)
 
+const build = async (): Promise<void> => {
+  await $`rm -rf dist`.text()
+  const exit = await $`bun run build:nopack`.text()
+  console.log(exit)
+}
+
+build()
+  .catch(console.error)
+
 const controller = new AbortController()
 
 const watcher = watch(path.resolve(__dirname, 'src'), { signal: controller.signal })
 
-watcher.on('change', (filename) => {
-  console.log(`Detected change in ${filename}`)
-  $`bun run build:nopack`.then(console.log).catch(console.error)
+watcher.on('change', (change, filename) => {
+  console.log(`Detected ${change} in ${typeof filename === 'string' ? filename : `Buffer : ${filename.toString()}`}`)
+  build()
+    .catch(console.error)
 })
 
 process.on('SIGINT', () => {
